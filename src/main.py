29ccafb5bad020892e2f5f557fe928eecebbd7d6
Pranprest <79233261+Pranprest@users.noreply.github.com>
2022-01-09 19:21:18 -0300

    General Refactoring
    Better replayKeyboard method,
    unified main file,
    Better comments.

--- src/main.py
+++ src/main.py
@@ -8,29 +8,35 @@
         Author: Pranprest
     ==
 """
-
-# WARNING: THIS FILE IS NOT FUNCITIONAL YET, IM TESTING IT
-
 from pynput import mouse, keyboard
-from time import sleep
+from time import sleep, time
 import logging
 logging.basicConfig(level=logging.DEBUG,
-                    filename="logfile.log", format='%(levelname)s:%(message)s')
+                    filename="..\\log\\logfile.log", format='%(levelname)s:%(message)s')
 
 
-def recordInput() -> list[list[tuple, enumerate], keyboard.Key]:
+def recordInput() -> dict[list, list, list]:
     """
         Records your mouse and keyboard input!
     """
-    user_input = []
+    initial_time = time()
+
+    user_input = {
+        "keyboard": [],
+        "mouse": [],
+        # TODO: Implement event timeline
+        # "event_timeline": []
+    }
 
     def on_click(x, y, button, pressed):
-        user_input.append([(x, y), button])
+        elapsed_time = round(time() - initial_time, 2)
+        user_input["mouse"].append([(x, y), button, elapsed_time])
         if pressed == keyboard.Key.esc:
             return False
 
     def on_press(key):
-        user_input.append([key])
+        elapsed_time = round(time() - initial_time, 2)
+        user_input["keyboard"].append((key, elapsed_time))
         if key == keyboard.Key.esc:
             return False
 
@@ -42,35 +48,38 @@ def recordInput() -> list[list[tuple, enumerate], keyboard.Key]:
 
     return user_input
 
-# Doesn't work yet!!
-# def replayMouse(mouseInput: list) -> None:
-#     for section in mouseInput:
-#         for element in section:
-#             if isinstance(element, tuple):
-#                 logging.debug(f"{element}, Is a Tuple")
-#             elif isinstance(element, Enum):
-#                 logging.debug(f"{element}, Is a Enum Object")
-#             break
+
+def replayMouse(InputList: list) -> None:
+    print("Replaying mouse input...")
+    raise NotImplementedError()
 
 
-def replayKeyboard(InputList: list) -> None:
+def replayKeyboard(InputList: list[tuple]) -> None:
+    print("Replaying keyboard input...")
     keyboard_controller = keyboard.Controller()
     for i in range(len(InputList)):  # Tuple w/ input and time
         key, currtime = InputList[i]
         if (InputList[i - 1] != InputList[-1]):
-            # Sleep X ammount of time (interval between last input and current one)
+            # Sleep for the ammount of time of the interval between last input and current one
             sleep(currtime - InputList[i - 1][1])
-        keyboard_controller.press(key)
-        keyboard_controller.release(key)
+        # TODO: Implement "shift", "control", "cmd" keys (not just one input only keys)
+        # TODO: Also somehow figure out a way to support weird non-american keyboard keys
+        try:
+            keyboard_controller.press(key)
+            keyboard_controller.release(key)
+        except Exception as e:
+            print("Unsupported key was pressed, check logfiles for more information")
+            logging.error(e)
+            exit()
 
 
 def main() -> None:
     try:
         user_input = recordInput()
-        logging.debug(user_input)
-        sleep(1)
-        replayKeyboard(user_input)
+        logging.info(user_input)
+        print("Script ran successfully!")
     except Exception as e:
+        print("An error ocourred during runtime, check logfiles for more information.")
         logging.error(e)
 
 
