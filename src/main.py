359726219b5fe68389021d614b01acc87010978d
Pranprest <79233261+Pranprest@users.noreply.github.com>
2022-01-12 00:41:18 -0300

    Properly stops mouselistener and better readability

--- src/main.py
+++ src/main.py
@@ -1,45 +1,54 @@
 """
     This script records your mouse and keyboard input for later re-doing them
     -> When you click, it records where you clicked and when you clicked
-    -> After recording, you can replay everything that you did during the recording
+    -> After recording, you can replay everything that you did during it
     -> Its like a TAS thing but for general use I guess?
 
     ==
         Author: Pranprest
     ==
 """
+from collections import namedtuple
+from typing import List
 from pynput import mouse, keyboard
 from time import sleep, time
 import logging
-logging.basicConfig(level=logging.DEBUG,
-                    filename="log\\logfile.log", format='%(levelname)s:%(message)s')
+
+import pynput
+
+logging.basicConfig(
+    level=logging.DEBUG, filename="log\\logfile.log", format="%(levelname)s:%(message)s"
+)
 
 
-def recordInput() -> dict[list, list, list]:
+def recordInput() -> tuple:
     """
-        Records your mouse and keyboard input!
+    Records your mouse and keyboard input!
     """
     initial_time = time()
 
-    user_input = {
-        "keyboard": [],
-        "mouse": [],
-        # TODO: Implement event timeline
-        # "event_timeline": []
-    }
+    Point = namedtuple("Point", "x y")
+    MouseInput = namedtuple("MouseInput", "pos button time")
+    KeyboardInput = namedtuple("KeyboardInput", "button time")
 
-    # TODO: Maybe using sets here would be a better idea than using lists within lists
+    MouseList = []
+    KeyboardList = []
+
     def on_click(x, y, button, pressed):
+        if not pressed:
+            return
         elapsed_time = round(time() - initial_time, 2)
-        user_input["mouse"].append([(x, y), button, elapsed_time])
-        if pressed == keyboard.Key.esc:
-            return False
+        MouseList.append(MouseInput(
+            pos=Point(x, y), button=button, time=elapsed_time))
+        print(MouseInput(
+            pos=Point(x, y), button=button, time=elapsed_time))
 
     def on_press(key):
-        elapsed_time = round(time() - initial_time, 2)
-        user_input["keyboard"].append((key, elapsed_time))
         if key == keyboard.Key.esc:
+            mouse_listener.stop()
             return False
+        elapsed_time = round(time() - initial_time, 2)
+        KeyboardList.append(KeyboardInput(key=key, time=elapsed_time))
 
     mouse_listener = mouse.Listener(on_click=on_click)
     mouse_listener.start()
@@ -47,39 +56,31 @@ def recordInput() -> dict[list, list, list]:
     with keyboard.Listener(on_press=on_press) as listener:
         listener.join()
 
-    return user_input
+    return MouseList, KeyboardList
 
 
 def replayMouse(InputList: list) -> None:
     print("Replaying mouse input...")
     mouse_controller = mouse.Controller()
-    # TODO: Make these more specific, maybe using sets or dictionaries (like: inputset.pos)(?)
-    for i in range(len(InputList)):
-        pos = InputList[i][0]
-        key = InputList[i][1]
-        curr_time = InputList[i][2]
-        if (InputList[i - 1] != InputList[-1]):
-            sleep(curr_time - InputList[i - 1][2])
-        try:
-            mouse_controller.position = pos
-            mouse_controller.press(key)
-            mouse_controller.release(key)
-        except Exception as e:
-            print("Unsupported key was pressed, check logfiles for more information")
-            logging.error(e)
-            exit()
+    for index, curr_input in enumerate(InputList):
+        pos, key, curr_time = curr_input
+        if InputList[index - 1] != InputList[-1]:
+            sleep(curr_time - InputList[index - 1][2])
+        mouse_controller.position = pos
+        mouse_controller.press(key)
+        mouse_controller.release(key)
+    return
 
 
 def replayKeyboard(InputList: list[tuple]) -> None:
     print("Replaying keyboard input...")
     keyboard_controller = keyboard.Controller()
-    for i in range(len(InputList)):  # Tuple w/ input and time
-        key, curr_time = InputList[i]
-        if (InputList[i - 1] != InputList[-1]):
+    for index, curr_input in enumerate(InputList):  # Tuple w/ input and time
+        key, curr_time = curr_input
+        if InputList[index - 1] != InputList[-1]:
             # Sleep for the ammount of time of the interval between last input and current one
-            sleep(curr_time - InputList[i - 1][1])
-        # TODO: Implement "shift", "control", "cmd" keys (not just one input only keys)
-        # TODO: Also somehow figure out a way to support weird non-american keyboard keys
+            sleep(curr_time - InputList[index - 1][2])
+        # TODO: Somehow figure out a way to support any other kind of key other than regular ASCII
         try:
             keyboard_controller.press(key)
             keyboard_controller.release(key)
@@ -87,13 +88,15 @@ def replayKeyboard(InputList: list[tuple]) -> None:
             print("Unsupported key was pressed, check logfiles for more information")
             logging.error(e)
             exit()
+    return
 
 
 def main() -> None:
     try:
-        user_input = recordInput()
-        logging.info(user_input)
-        print("Script ran successfully!")
+        mouselist, keyboardlist = recordInput()
+        logging.debug(f"{mouselist}, {keyboardlist}")
+        replayMouse(mouselist)
+        # replayKeyboard(keyboardlist)
     except Exception as e:
         print("An error ocourred during runtime, check logfiles for more information.")
         logging.error(e)
